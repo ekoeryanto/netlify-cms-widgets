5a90d514941a6718fd6c96c2921dcc8ab077947d
Eko Eryanto <ekoeryanto@gmail.com>
2018-05-07 16:55:14 +0700

    chore: rename packages to widgets in plopfile

--- plopfile.js
+++ plopfile.js
@@ -18,47 +18,47 @@ module.exports = function (plop) {
       [
         {
           type: 'add',
-          path: 'packages/{{kebabCase name}}/package.json',
+          path: 'widgets/{{kebabCase name}}/package.json',
           templateFile: 'plop-templates/widget/package.json.hbs'
         },
         {
           type: 'add',
-          path: 'packages/{{kebabCase name}}/src/index.js',
+          path: 'widgets/{{kebabCase name}}/src/index.js',
           templateFile: 'plop-templates/widget/index.js.hbs'
         },
         {
           type: 'add',
-          path: 'packages/{{kebabCase name}}/src/Control.js',
+          path: 'widgets/{{kebabCase name}}/src/Control.js',
           templateFile: 'plop-templates/widget/control.js.hbs'
         },
         {
           type: 'add',
-          path: 'packages/{{kebabCase name}}/src/Preview.js',
+          path: 'widgets/{{kebabCase name}}/src/Preview.js',
           templateFile: 'plop-templates/widget/preview.js.hbs'
         },
         {
           type: 'add',
-          path: 'packages/{{kebabCase name}}/README.md',
+          path: 'widgets/{{kebabCase name}}/README.md',
           templateFile: 'plop-templates/widget/README.md.hbs'
         },
         {
           type: 'add',
-          path: 'packages/{{kebabCase name}}/.babelrc',
+          path: 'widgets/{{kebabCase name}}/.babelrc',
           templateFile: 'plop-templates/widget/.babelrc.hbs'
         },
         {
           type: 'add',
-          path: 'packages/{{kebabCase name}}/rollup.config.js',
+          path: 'widgets/{{kebabCase name}}/rollup.config.js',
           templateFile: 'plop-templates/widget/rollup.config.js.hbs'
         },
         {
           type: 'add',
-          path: 'packages/{{kebabCase name}}/public/index.html',
+          path: 'widgets/{{kebabCase name}}/public/index.html',
           templateFile: 'plop-templates/widget/index.html.hbs'
         },
         {
           type: 'add',
-          path: 'packages/{{kebabCase name}}/public/config.yml',
+          path: 'widgets/{{kebabCase name}}/public/config.yml',
           templateFile: 'plop-templates/widget/config.yml.hbs'
         }
       ].filter(Boolean)
