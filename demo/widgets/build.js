89222ffe61bb5ffc5dbc01d653b1a0d937abba9f
Eko Eryanto <ekoeryanto@gmail.com>
2018-06-13 03:30:56 +0700

    chore: change cache dir location

--- demo/widgets/build.js
+++ demo/widgets/build.js
@@ -1,26 +1,23 @@
-// eslint-disable import/no-extraneous-dependencies
 process.env.NODE_ENV = 'production';
 // eslint-disable-next-line
 const Bundler = require('parcel-bundler');
 const { join } = require('path');
 const fs = require('fs');
 
-const { NETLIFY_REPO_DIR, NETLIFY_BUILD_BASE } = process.env;
-const cacheDir = NETLIFY_BUILD_BASE
-  ? join(NETLIFY_BUILD_BASE, 'cache', 'widgets')
-  : join(NETLIFY_REPO_DIR || '.', 'node_modules', '.cache');
+const rootDir = (...paths) => join(__dirname, '..', '..', ...paths);
+const baseDir = (...paths) => join(__dirname, ...paths);
 
 // Entrypoint file location
-const file = join(__dirname, './index.html');
+const file = baseDir('./index.html');
 
 // Bundler options
 const options = {
-  outDir: './dist',
+  outDir: rootDir('dist'),
   outFile: 'index.html',
   publicUrl: './',
   watch: false,
   cache: true,
-  cacheDir,
+  cacheDir: rootDir('node_modules/.cache/demo/widgets'),
   minify: true,
   target: 'browser',
   https: false,
@@ -35,5 +32,5 @@ const options = {
 const bundler = new Bundler(file, options);
 
 bundler.bundle().then(() => {
-  fs.createReadStream(join(__dirname, 'config.yml')).pipe(fs.createWriteStream(join(options.outDir, 'config.yml')));
+  fs.createReadStream(baseDir('config.yml')).pipe(fs.createWriteStream(join(options.outDir, 'config.yml')));
 });
